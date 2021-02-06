0936327e7c3859d719ffb191ebac165cdf9e5662
Sami <samibutler1992@gmail.com>
2021-02-06 14:35:19 -0700

    adding db and seed files

---
+++ app/seeds/OBD.py
@@ -0,0 +1,31 @@
+from werkzeug.security import generate_password_hash
+from app.models.user import db, OBD
+
+
+def seed_OBD():
+
+    OBD0 = OBD(
+        carId=1,
+        code='P0420',
+        urgency='2',
+        urgency_desc='Repair immediately if drivability issues are present. Threat to essential system components if not repaired as soon as possible.',
+        effect_on_vehicle='This condition will prevent the vehicle from running at its optimum efficiency and fuel economy may suffer.',
+        responsible_system='Sensors indicate the catalytic converter is not reducing exhaust gas emissions properly.',
+        layman_definition='Catalyst System Efficiency Below Threshold (Bank 1)',
+        tech_definition='Catalyst System Low Efficiency (Bank 1)',
+        )
+
+
+    db.session.add(OBD0)
+
+    db.session.commit()
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and resets
+# the auto incrementing primary key
+
+
+def undo_OBD():
+    db.session.execute('TRUNCATE map_bookmarks CASCADE;')
+    db.session.commit()
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .cars import seed_cars, undo_cars
+from .mapBookmarks import seed_map_bookmarks, undo_map_bookmarks
+from .OBD import seed_OBD, undo_OBD
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,9 +13,13 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_cars()
+    seed_map_bookmarks()
+    seed_OBD()
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
     undo_cars()
+    undo_map_bookmarks()
+    undo_OBD()
