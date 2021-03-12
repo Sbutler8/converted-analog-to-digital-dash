298a2450609fe99c1d866ff6e835f54a7ec33ed3
Sami <samibutler1992@gmail.com>
2021-03-11 22:52:37 -0700

    Adding screen size warning on dash

---
+++ react-app/src/components/ScreenSizeWarning/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ScreenSizeWarning.css';
+
+const ScreenSizeWarning = () => {
+    return (
+        <>
+            <div className="screensize-warning-container">
+                <i className="fas fa-exclamation-triangle"></i>
+                <span className="warning-text">{`Looks like you are utlizing this app using a screen size of ${window.innerWidth}px by ${window.innerHeight}px. Please note this application is specifically designed for an Arduino 7" touchscreen with a screen size of 480px by 800px.`}</span>
+            </div>
+        </>
+    )
+
+}
+
+export default ScreenSizeWarning;
